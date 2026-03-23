4f3c1e9399201ae9260fd0fb6b1a12d15caa5e7e
Vedant Limaye <109674630+Vedantl39@users.noreply.github.com>
2026-03-23 11:52:44 +0000

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,6 @@
--- FACT TABLE (core data)
-CREATE TABLE fact_flights (
-    flight_id SERIAL PRIMARY KEY,
+CREATE TABLE flights (
+    year INT,
+    month INT,
     flight_date DATE,
     airline VARCHAR(10),
     origin_airport VARCHAR(10),
@@ -9,26 +9,5 @@ CREATE TABLE fact_flights (
     arrival_delay FLOAT,
     cancelled BOOLEAN,
     diverted BOOLEAN,
-    distance FLOAT,
-    is_delayed_15 BOOLEAN
-);
-
--- AIRPORT DIMENSION
-CREATE TABLE dim_airports (
-    airport_code VARCHAR(10) PRIMARY KEY,
-    city_name VARCHAR(100)
-);
-
--- AIRLINE DIMENSION
-CREATE TABLE dim_airlines (
-    airline_code VARCHAR(10) PRIMARY KEY
-);
-
--- DATE DIMENSION
-CREATE TABLE dim_date (
-    flight_date DATE PRIMARY KEY,
-    year INT,
-    month INT,
-    day INT,
-    day_of_week INT
+    distance FLOAT
 );
