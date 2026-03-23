a30fd0954bc4970847ca052676d22cf1a2edfde7
Vedant Limaye <109674630+Vedantl39@users.noreply.github.com>
2026-03-23 11:58:16 +0000

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,13 +1,27 @@
-CREATE TABLE flights (
+CREATE TABLE fact_flights (
+    flight_id SERIAL PRIMARY KEY,
     year INT,
     month INT,
     flight_date DATE,
-    airline VARCHAR(10),
+    op_carrier_fl_num VARCHAR(20),
     origin_airport VARCHAR(10),
     destination_airport VARCHAR(10),
     departure_delay FLOAT,
     arrival_delay FLOAT,
     cancelled BOOLEAN,
     diverted BOOLEAN,
-    distance FLOAT
+    distance FLOAT,
+    is_delayed_15 BOOLEAN
+);
+
+CREATE TABLE dim_airports (
+    airport_code VARCHAR(10) PRIMARY KEY
+);
+
+CREATE TABLE dim_date (
+    flight_date DATE PRIMARY KEY,
+    year INT,
+    month INT,
+    day INT,
+    day_of_week INT
 );
