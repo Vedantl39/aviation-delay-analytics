b75efa88856d09645ae52b8d333171b661678d28
Vedant Limaye <109674630+Vedantl39@users.noreply.github.com>
2026-03-23 12:04:31 +0000

    Update load_to_postgres.py

--- scripts/load_to_postgres.py
+++ scripts/load_to_postgres.py
@@ -1,19 +1,13 @@
 import pandas as pd
 from sqlalchemy import create_engine
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Load cleaned data
 df = pd.read_csv("data/processed/flights_clean.csv")
 
-# Create connection
-engine = create_engine(
-    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-)
+# Create connection (update with your credentials)
+engine = create_engine("postgresql://postgres:password@localhost:5432/aviation_db")
 
-# Load into fact table
+# Load into fact_flights
 df.to_sql("fact_flights", engine, if_exists="replace", index=False)
 
-print("Data loaded into PostgreSQL.")
+print("Data loaded into PostgreSQL successfully.")
